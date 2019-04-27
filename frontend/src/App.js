5d5196af1315bd6c58ba7bcfb60550dd1aec8ba5
Diego Guevara <diegogb0113@gmail.com>
2019-04-27 18:23:32 -0500

    Implementación de autenticacion y autorización para ingreso a la aplicación

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,76 @@
+import './main.scss';
+
 import React, { Component } from 'react';
-import './App.css';
-import Calendar from './Calendar'
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-class App extends Component {
+import { auth } from './actions';
+import calendarApp from './reducers';
+
+import NotFound from '';
+import Register from '';
+import Login from '';
+import Calendar from './components/Calendar'
+
+let store = createStore(calendarApp, applyMiddleware(thunk));
+
+class RootContainerComponent extends Component {
+  componentDidMount() {
+    this.props.loadUser();
+  }
+  PrivateRoute = ({ Component: ChildComponent, ...rest }) => {
+    return <Route {...rest} render={props => {
+      if (this.props.auth.isLoading) {
+        return <em>Loading...</em>;
+      }
+      else if (!this.props.auth.isAuthenticated) {
+        return <Redirect to="/login" />;
+      }
+      else {
+        return <ChildComponent {...props} />;
+      }
+    }} />
+  }
   render() {
+    let { PrivateRoute } = this;
     return (
-    <Calendar />
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute exact path="/" component={Calendar} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadUser: () => {
+      return dispatch(auth.loadUser());
+    }
+  }
+}
+
+let RootContainer = connect(mapStateToProps, mapDispatchToProps)(RootContainerComponent);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <RootContainer />
+      </Provider>
+    )
+  }
+}

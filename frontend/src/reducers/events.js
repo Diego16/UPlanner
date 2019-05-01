b2a38ad7ab06aa3e4cb89ac0cd8bcdaeaac4301b
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-05-01 13:58:09 -0500

    reducers de eventos

---
+++ frontend/src/reducers/events.js
@@ -0,0 +1,28 @@
+const initialState = [];
+
+
+export default function events(state=initialState, action) {
+    let eventList = state.slice();
+
+    switch (action.type) {
+
+        case 'FETCH_EVENTS':
+            return [...state, ...action.events];
+
+        case 'ADD_EVENT':
+            return [...state, action.note];
+
+        case 'UPDATE_EVENT':
+            let noteToUpdate = eventList[action.index]
+            noteToUpdate.text = action.note.text;
+            eventList.splice(action.index, 1, noteToUpdate);
+            return eventList;
+
+        case 'DELETE_EVENT':
+            eventList.splice(action.index, 1);
+            return eventList;
+
+        default:
+            return state;
+    }
+}

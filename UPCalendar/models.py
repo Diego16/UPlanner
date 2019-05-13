a1468eebe3cf1e27d5a3abde8f35751dc0b7f293
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-05-12 21:15:36 -0500

    agregados atributos de eventos y tareas

--- UPCalendar/models.py
+++ UPCalendar/models.py
@@ -14,13 +14,16 @@ class Student(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=100, null=True)
     description = models.TextField(max_length=1000, null=True)
-    allDay = models.BooleanField(null=True)
+    allDay = models.BooleanField(default=False)
     start = models.DateTimeField()
     end = models.DateTimeField()
     url = models.URLField(null=True)
     user = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
+    rrule = models.CharField(max_length=200, null=True)
 
 
 class Task(models.Model):
     title = models.CharField(max_length=100, null=True)
     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
+    start = models.DateTimeField(null=True)
+    end = models.DateTimeField(null=True)

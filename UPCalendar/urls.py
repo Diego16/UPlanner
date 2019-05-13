f1b8b9bbd5801902d4a98765f9aea7ae2f728faf
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-05-12 21:16:31 -0500

    Urls corregidas

--- UPCalendar/urls.py
+++ UPCalendar/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students', views.StudentsAPI.as_view()),
-    path('events', views.EventsAPI.as_view()),
-    path('create_event', views.CreateEventAPI.as_view()),
-    path('register', views.RegistrationAPI.as_view()),
-    path('login', views.LoginAPI.as_view()),
-    path('user', views.UserAPI.as_view())
+    path('students/', views.StudentsAPI.as_view()),
+    path('events/', views.EventsAPI.as_view()),
+    path('create_event/', views.CreateEventAPI.as_view()),
+    path('register/', views.RegistrationAPI.as_view()),
+    path('login/', views.LoginAPI.as_view()),
+    path('user/', views.UserAPI.as_view())
 
 ]

0826959f8e8037c47157ac658403fb5bc2db9577
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-05-22 10:03:09 -0500

    script para la creación de la base de datos

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+if [ $# -eq 0 ]
+  then
+    mysql -h localhost -P 3306 -uuplanner_user -puplanner_db_pwd -e "drop database if exists uplanner; create database uplanner;"
+    python manage.py migrate
+fi

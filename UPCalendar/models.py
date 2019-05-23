453bc0b8c60e81266ba2b887e4978ac6bf484c76
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-05-23 01:15:11 -0500

    Modificaciones a Event y Task

--- UPCalendar/models.py
+++ UPCalendar/models.py
@@ -7,10 +7,10 @@ from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     workTime = models.IntegerField(null=True, default=2)
-    sleepTime = models.TimeField(null=True, default=datetime.time(21, 00))
-    wakeUpTime = models.TimeField(null=True, default=datetime.time(7, 00))
+    sleepTime = models.DateTimeField(null=True, default=datetime.datetime(2017, 11, 28, 21, 00))
+    wakeUpTime = models.DateTimeField(null=True, default=datetime.datetime(2017, 11, 28, 7, 00))
     workOnWeekends = models.BooleanField(default=True)
 
 
@@ -20,7 +20,7 @@ class Event(models.Model):
     allDay = models.BooleanField(default=False)
     start = models.DateTimeField()
     end = models.DateTimeField()
-    color = models.CharField(max_length=7, default="#325d88")
+    color = models.CharField(max_length=7, default="#7B7B7B")
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     recurrent = models.BooleanField(default=False)
     rrule = models.CharField(max_length=200, null=True)
@@ -28,7 +28,7 @@ class Event(models.Model):
 
 class Task(models.Model):
     title = models.CharField(max_length=100, null=True)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
     start = models.DateTimeField(null=True)
-    end = models.DateTimeField(null=True)
     description = models.TextField(max_length=1000, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    color = models.CharField(max_length=7, default="#B71C1C")

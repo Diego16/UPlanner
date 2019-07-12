3be9b54a538eaef350ac70ca5440055e43409e7d
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-07-12 15:15:55 -0500

    Django version updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ $# -eq 0 ]
+if [[ $# -eq 0 ]]
   then
     mysql -h localhost -P 3306 -uuplanner_user -puplanner_db_pwd -e "drop database if exists uplanner; create database uplanner;"
     python manage.py migrate
